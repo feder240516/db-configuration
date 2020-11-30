dd32b2d7fb518c76339b886e2f989ebb1f97b0c6
Iancrix <ianchonaramirez@gmail.com>
2020-11-30 00:02:09 -0500

    Modified IDatabase interface

--- src/main/java/benchmarking/IDatabase.java
+++ src/main/java/benchmarking/IDatabase.java
@@ -7,6 +7,6 @@ import ai.libs.jaicore.components.api.IComponentInstance;
 public interface IDatabase {
 	public void initiateServer(IComponentInstance component);
 	public void stopServer();
-	public double benchmarkQuery(String query);
+	public double benchmarkQuery(int numTest);
 	public Connection getConnection(IComponentInstance component);
 }
