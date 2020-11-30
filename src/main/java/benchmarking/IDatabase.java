16d1a6b34327756a4b7360c88d33a7cb56875f1a
Iancrix <ianchonaramirez@gmail.com>
2020-11-30 00:05:01 -0500

    Modified IDatabase interface again

--- src/main/java/benchmarking/IDatabase.java
+++ src/main/java/benchmarking/IDatabase.java
@@ -8,5 +8,5 @@ public interface IDatabase {
 	public void initiateServer(IComponentInstance component);
 	public void stopServer();
 	public double benchmarkQuery(int numTest);
-	public Connection getConnection(IComponentInstance component);
+	public Connection getConnection();
 }
