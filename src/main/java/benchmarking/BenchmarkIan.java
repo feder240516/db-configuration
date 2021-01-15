271101512c59fb980ffec741fa4cb170cafaaef2
Iancrix <ianchonaramirez@gmail.com>
2021-01-14 23:13:07 -0500

    Added implementation of abstract methods

--- src/main/java/benchmarking/BenchmarkIan.java
+++ src/main/java/benchmarking/BenchmarkIan.java
@@ -3,10 +3,18 @@ package benchmarking;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 import ai.libs.jaicore.components.api.IComponent;
 import ai.libs.jaicore.components.api.IComponentInstance;
@@ -20,33 +28,75 @@ public class BenchmarkIan {
 		BenchmarkIan benchmark = new BenchmarkIan();
 		
 		IComponent comp = new Component("MariaDB");
-		
 		Map<String, String> parameterValues = new HashMap<>();
 		parameterValues.put("OPTIMIZER_SEARCH_DEPTH", "47");
 		Map<String, List<IComponentInstance>> reqInterfaces = new HashMap<>(); 
-		
 		IComponentInstance i1 = new ComponentInstance(comp, parameterValues, reqInterfaces);
+		//ComponentSerialization serializer = new ComponentSerialization();
 		
-		ComponentSerialization serializer = new ComponentSerialization();
-		System.out.println(serializer.serialize(i1));
+		IComponent comp2 = new Component("MariaDB");
+		Map<String, String> parameterValues2 = new HashMap<>();
+		parameterValues2.put("OPTIMIZER_SEARCH_DEPTH", "35");
+		Map<String, List<IComponentInstance>> reqInterfaces2 = new HashMap<>(); 
+		IComponentInstance i2 = new ComponentInstance(comp, parameterValues2, reqInterfaces2);
 		
-		/*
-		DBHandlerIan dbHandler = benchmark.launchDatabase();
-		benchmark.benchmark(i1);
-		dbHandler.Stop();
-		*/
+		double value1 = benchmark(i1, 3);
+		double value2 = benchmark(i2, 3);
 		
-		/*
-		MariaDBHandler mariaDBHandler = new MariaDBHandler();
-		mariaDBHandler.initiateServer(i1);
-		System.out.println(mariaDBHandler.benchmarkQuery(0));
-		mariaDBHandler.stopServer();
-		*/
+		System.out.println("value 1" + value1);
+		System.out.println("value 2" + value2);
 		
-		MariaDBHandler mariaDBHandler = new MariaDBHandler();
-		mariaDBHandler.initiateServer(i1);
-		System.out.println(mariaDBHandler.benchmarkQuery(0));
-		mariaDBHandler.stopServer();
+		/*int port = mariaDBHandler.initiateServer(i1);
+		double value = mariaDBHandler.benchmarkQuery(1, port);
+		System.out.println("Value: " + value);
+		mariaDBHandler.stopServer(port);*/
+	}
+	
+	public static double benchmark(IComponentInstance component, int numTests) {
+		int testNumber = 1;
+		ADatabaseHandle mariaDBHandler = new MariaDBHandler();
+		
+		ExecutorService executor = (ExecutorService) Executors.newFixedThreadPool(3);
+		 
+        List<TestExecution> taskList = new ArrayList<>();
+        for (int i = 0; i < numTests; i++) {
+        	TestExecution task = new TestExecution(mariaDBHandler,component,testNumber);
+            taskList.add(task);
+        }
+         
+        //Execute all tasks and get reference to Future objects
+        List<Future<Double>> resultList = null;
+ 
+        try {
+            resultList = executor.invokeAll(taskList);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+ 
+        executor.shutdown();
+ 
+        System.out.println("\n========Printing the results======");
+        
+        DescriptiveStatistics ds = new DescriptiveStatistics();
+         
+        for (int i = 0; i < resultList.size(); i++) {
+            Future<Double> future = resultList.get(i);
+            try {
+                double result = future.get();
+                ds.addValue(result);
+                System.out.println(String.format("Adding value %f", result));
+                
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        System.out.println(String.format("min value: %f",ds.getMin()));
+        System.out.println(String.format("max value: %f",ds.getMax()));
+        System.out.println(String.format("std value: %f",ds.getStandardDeviation()));
+        System.out.println(String.format("mean value: %f",ds.getMean()));
+        
+		return ds.getMean();
 	}
 	
 }
--- src/main/java/benchmarking/MariaDBHandler.java
+++ src/main/java/benchmarking/MariaDBHandler.java
@@ -2,7 +2,9 @@ package benchmarking;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.sql.Connection;
@@ -10,25 +12,30 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import ai.libs.jaicore.components.api.IComponentInstance;
 
-public class MariaDBHandler implements IDatabase {
+public class MariaDBHandler extends ADatabaseHandle {
 
-	Process process;
-	public boolean ExecuteCommand(String cmdLine) {
+	HashMap<Integer, String> directories = new HashMap<>();
+	
+	public MariaDBHandler() {
+		super(new int[]{3306, 3307, 3308});
+		directories.put(3306, "C:/Users/WIN/Desktop/MariaDB_Instances/instance1");
+		directories.put(3307, "C:/Users/WIN/Desktop/MariaDB_Instances/instance2");
+		directories.put(3308, "C:/Users/WIN/Desktop/MariaDB_Instances/instance3");
+	}
+	
+	public boolean executeCommand(String cmdLine) {
 		ProcessBuilder pb = new ProcessBuilder();
 		pb.command("cmd.exe", "/c", cmdLine);
-		//pb.command("mysqld.exe");
 		
 		boolean success;
 		try {
-			process = pb.start();
-			System.out.println("Waiting");
-			TimeUnit.SECONDS.sleep(1);
-
+			pb.start();
 			success = true;
 		}catch (Exception e){
 			e.printStackTrace();
@@ -37,33 +44,7 @@ public class MariaDBHandler implements IDatabase {
 		return success;
 	}
 	
-	public boolean ApplyParameters(IComponentInstance component) {
-		Map<String, String> parameters = component.getParameterValues();
-		String sqlBase = "";
-		
-		boolean success;
-		for(String keyParam : parameters.keySet()) {
-			String valueParam = parameters.get(keyParam);
-			
-			sqlBase += "SET " + keyParam + " = " + valueParam + "; ";
-		}
-		
-		Connection conn = getConnection();
-		try {
-			PreparedStatement ps = conn.prepareStatement(sqlBase);
-			ps.executeQuery();
-			ps.close();
-			conn.close();
-			
-			success = true;
-		}catch(SQLException e) {
-			e.printStackTrace();
-			success = false;
-		}
-		
-		return success;
-	}
-
+	/*
 	@Override
 	public void initiateServer(IComponentInstance component) {
 		String cmdStart = "mysqld";//"net start mariadb";
@@ -75,12 +56,13 @@ public class MariaDBHandler implements IDatabase {
 		isSuccessful = ApplyParameters(component);
 		msg = (isSuccessful) ? "Se aplicaron los parametros": "No se aplicaron los parametros";
 		System.out.println(msg);
-	}
+	}*/
 
+	/*
 	@Override
 	public void stopServer() {
 		process.destroy();
-		System.out.println("Se detuvo MariaDB");
+		System.out.println("Se detuvo MariaDB");*/
 		
 		/*
 		String cmdStop = "mysqladmin -u root --password=12345 shutdown";//"net stop mariadb";
@@ -88,8 +70,9 @@ public class MariaDBHandler implements IDatabase {
 	
 		String msg = (isSuccessful) ? "Se detuvo MariaDB": "No se detuvo MariaDB";
 		System.out.println(msg);*/
-	}
+	//}
 
+	/*
 	@Override
 	public double benchmarkQuery(int numTest) {
 		String sqlBase = "SELECT employees.first_name, employees.hire_date, "
@@ -118,9 +101,9 @@ public class MariaDBHandler implements IDatabase {
 		}
 		
 		return Long.valueOf(endDate.getTime() - startDate.getTime()).doubleValue()/1000;
-	}
-
-	@Override
+	}*/
+	
+	/*
 	public Connection getConnection() {
 		String db = "employees";
 		String url = "jdbc:mariadb://localhost:3306/" + db;
@@ -136,6 +119,154 @@ public class MariaDBHandler implements IDatabase {
 			System.out.println("Connection to MariaDB unsuccessful");
 		}
 		return conn;
+	}*/
+	
+	private boolean ApplyParameters(IComponentInstance component, int port) {
+		Map<String, String> parameters = component.getParameterValues();
+		String sqlBase = "";
+		
+		boolean success;
+		for(String keyParam : parameters.keySet()) {
+			String valueParam = parameters.get(keyParam);
+			
+			sqlBase += "SET " + keyParam + " = " + valueParam + "; ";
+		}
+		
+		Connection conn = getConnection(port);
+		try {
+			PreparedStatement ps = conn.prepareStatement(sqlBase);
+			ps.executeQuery();
+			ps.close();
+			conn.close();
+			
+			success = true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			success = false;
+		}
+		
+		return success;
+	}
+
+	@Override
+	public int initiateServer(IComponentInstance component) {
+		System.out.println("Starting server...");
+		
+		int port = useNextAvailablePort();
+		//String[] comandoArray = getStartCommand(component, port);
+		System.out.println("Starting server on port: " + port);
+		
+		String cmdLine = "mysqld --defaults-file=.cnf";
+		
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.directory(new File(directories.get(port)));
+		
+		processBuilder.command("cmd.exe", "/c", cmdLine);
+		
+		try {			
+			Process process = processBuilder.start();
+			InputStream inStream = process.getInputStream();
+			InputStream errStream = process.getErrorStream();
+			
+			inStream.close();
+			errStream.close();
+			
+			Connection conn = null;
+			// tries multiple connections to database
+			for(int i = 0; i < MAX_CONNECTION_RETRIES && conn == null; ++i) {
+				System.out.println("Trying to establish a connection...");
+				// wait 5 seconds to allow server to initiate
+				TimeUnit.SECONDS.sleep(5);
+				conn = getConnection(port);
+			}
+			
+			if (conn != null && !conn.isClosed()) {
+				setupInitedDB(component, port);
+				String msg = String.format("Server on port %d has been inited", port);
+				System.out.println(msg);
+			}else {
+				throw new SQLException(String.format("Could not connect to database after %d tries", MAX_CONNECTION_RETRIES));
+			}
+			
+		}catch(IOException | InterruptedException | SQLException  e) {
+			e.printStackTrace();
+		}
+		
+		return port;
+	}
+
+	@Override
+	protected String[] getStartCommand(IComponentInstance component, int port) {
+		String directory = directories.get(port);
+		String[] cmdStart = {"mysqld --defaults-file=.cnf"};
+		return cmdStart;
+	}
+
+	@Override
+	protected String getDbDirectory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void createAndFillDatabase(int port) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupInitedDB(IComponentInstance component, int port) {
+		// TODO Auto-generated method stub
+		boolean isSuccessful = ApplyParameters(component, port);
+		String msg = (isSuccessful) ? "Se aplicaron los parametros": "No se aplicaron los parametros";
+		System.out.println(msg);
+	}
+
+	@Override
+	public void stopServer(int port) {
+		System.out.println("Stopping server");
+		
+		String cmdLine = String.format("mysqladmin -u root --password= --port=%d shutdown", port);
+		
+		Connection conn = getConnection(port);
+		try {
+			if (conn != null && !conn.isClosed()) conn.close();
+			//String[] cmdStop = {cmdLine};
+			ProcessBuilder processBuilder = new ProcessBuilder();
+			processBuilder.command("cmd.exe", "/c", cmdLine);
+			processBuilder.start().waitFor();
+			
+			String msg = String.format("The server on port %d was stopped successfully", port);
+			System.out.println(msg);
+			
+		} catch (IOException | SQLException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected String getQueryCommand(int numTest) {
+		// TODO Auto-generated method stub
+		switch(numTest) {
+		case 1:
+			return "SELECT employees.first_name, employees.hire_date, "
+					+ "dept_emp.from_date, dept_emp.to_date "
+					+ "FROM employees INNER JOIN dept_emp "
+					+ "ON employees.emp_no =dept_emp.emp_no;";
+		default:
+			throw new RuntimeException("Test inexistente");
+		}
+	}
+
+	@Override
+	protected String getConnectionString(int port) {
+		String dbName = "employees";
+		String user = "root";
+		String password = "";
+		
+		String dbUrl = String.format("jdbc:mariadb://localhost:%d/%s?user=%s&password=%s", port, dbName, user, password);
+		return dbUrl;
 	}
 
 }
