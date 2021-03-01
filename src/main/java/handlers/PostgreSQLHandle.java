2018a4b7932c126249427daec67b3e629464f9d7
Federico Reina <federicoreinavinchira@yahoo.com>
2021-02-28 22:07:07 -0500

    restructure for postgresql

--- src/main/java/handlers/PostgreSQLHandle.java
+++ src/main/java/handlers/PostgreSQLHandle.java
@@ -11,123 +11,66 @@ import java.util.UUID;
 import org.apache.commons.io.FileUtils;
 
 import ai.libs.jaicore.components.api.IComponentInstance;
+import exceptions.UnavailablePortsException;
 import helpers.TestDescription;
 
 public class PostgreSQLHandle extends ADatabaseHandle {
-public static final String DATABASE_PAGE_SIZE = "DATABASE_PAGE_SIZE";
-	
-	// HashMap<Integer, String> dbNames;
-	HashMap<Integer, String> directories = new HashMap<>();
 	String instancesPath = "D:\\Bibliotecas\\Documents\\_Programming_Assets\\Postgresql\\instances";
 	String baseDataPath = "D:\\Bibliotecas\\Documents\\_Programming_Assets\\Postgresql\\data";
 	
-	public PostgreSQLHandle(int[] portsToUse, TestDescription testDescription, int allowedThreads) {
-		super(portsToUse,allowedThreads,testDescription);
-		initHandler();
-	}
-	
-	private void initHandler() {
-		// dbNames = new HashMap<>();
-		initInstances(this.portsToUse);
-	}
-	
-	protected void initInstances(int[] portsToUse) {
-		File instancesDir = new File(instancesPath);
-		
-		if (!instancesDir.exists()) instancesDir.mkdirs();
-		
-		String[] instances = instancesDir.list();
-		int numberOfInstances = instances.length;
-		
-		for(int i = 0; i < numberOfInstances; i++){
-			for(int port: portsToUse) {
-				if(!directories.containsKey(port)) { 
-					directories.put(port, instancesDir.getPath() + "/" + instances[i]); 
-					break;
-				}
-			}
-		}
-		
-		for(int port: portsToUse) {
-			if(!directories.containsKey(port)) { 
-				createDBInstance(port);
-			}
-		}
-	}
-	
-	public void createDBInstance(int port) {
-		File dataDir = new File(baseDataPath);
-		
-		String instancePath = instancesPath + "/" + UUID.randomUUID();
-		File destDir = new File(instancePath);
-		
-		try {
-		    FileUtils.copyDirectory(dataDir, destDir);
-		    directories.put(port, instancePath);
-		    System.out.println("The instance " + instancePath + " on port " + port + " was created");
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
+	public PostgreSQLHandle(IComponentInstance componentInstance) throws UnavailablePortsException, IOException, SQLException, InterruptedException {
+		super(componentInstance);
 	}
 	
 	@Override
-	protected void createAndFillDatabase(int port) {}
+	protected void createAndFillDatabase() {}
 
 	@Override
-	protected String[] getStartCommand(IComponentInstance component, int port) {
+	protected String[] getStartCommand() {
 		String postgresqlHome = System.getenv("POSTGRESQL_HOME");
 		System.out.println(String.format("Running in port %d", port));
 		if (postgresqlHome == null || postgresqlHome.equals("")) throw new RuntimeException("Environment Var postgresqlHome must be configured to test PostgreSQL");
-		String[] comandoArray = {"\"" + postgresqlHome + "/bin/pg_ctl" + "\"", "-D", directories.get(port), "-l", directories.get(port) + "/log.txt", "-o", String.format("\"-F -p %d\"", port), "start"};
+		String[] comandoArray = {"\"" + postgresqlHome + "/bin/pg_ctl" + "\"", "-D", createdInstancePath, "-l", createdInstancePath + "/log.txt", "-o", String.format("\"-F -p %d\"", port), "start"};
 		return comandoArray;
 	}
 
 	@Override
-	protected String getDbDirectory(int port) {
-		return directories.get(port);
+	protected String getDbDirectory() {
+		return null;
 	}
 
 	@Override
-	protected void setupInitedDB(IComponentInstance component, int port) {
-		
+	protected void setupInitedDB() {
+		// TODO: Search for parameter configurations
 	}
 
 	@Override
-	public void stopServer(int port) {
+	public void stopServer() {
 		System.out.println("Stopping server");
-		Connection conn = getConnection(port);
 		try {
-			if (conn != null && !conn.isClosed()) conn.close();
 			String postgresqlHome = System.getenv("POSTGRESQL_HOME");
-			String[] comandoArray = {postgresqlHome + "/bin/pg_ctl", "-D", directories.get(port), "-l", directories.get(port) + "/log.txt", String.format("-o \"-F -p %d\"", port), "stop"};
+			String[] comandoArray = {postgresqlHome + "/bin/pg_ctl", "-D", createdInstancePath, "-l", createdInstancePath + "/log.txt", String.format("-o \"-F -p %d\"", port), "stop"};
 			ProcessBuilder processBuilder = new ProcessBuilder(comandoArray);
 			processBuilder.start().waitFor();
 			
-		} catch (IOException | SQLException | InterruptedException e) {
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
-		}
-	}
-
-	@Override
-	protected String getQueryCommand(int numTest) {
-		switch(numTest) {
-		case 1:
-			return "select * from restaurants join workers on restaurants.id = workers.restid";
-		default:
-			throw new RuntimeException("Test inexistente");
 		}
 	}
 	
 	@Override
-	protected String getConnectionString (int port) {
-		String directory = directories.get(port);
+	protected String getConnectionString () {
 		String dbUrl = String.format("jdbc:postgresql://localhost:%d/%s?user=%s&password=%s", port, "employees", "feder", "root");
 		return dbUrl;
 	}
 
 	@Override
-	public void cleanup() {
-		// TODO Auto-generated method stub
-		
+	protected String getInstancesPath() {
+		return instancesPath;
+	}
+
+	@Override
+	protected String getBasePath() {
+		return baseDataPath;
 	}
 }
