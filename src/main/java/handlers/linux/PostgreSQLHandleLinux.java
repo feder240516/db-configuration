66056f8fdf819315f10d839f0b7b9119402a1aa1
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-17 02:04:59 -0500

    fixed postgres test

--- src/main/java/handlers/linux/PostgreSQLHandleLinux.java
+++ src/main/java/handlers/linux/PostgreSQLHandleLinux.java
@@ -16,17 +16,20 @@ public class PostgreSQLHandleLinux extends PostgreSQLHandle {
 	
 	@Override
 	public void createDBInstance() throws IOException {
-		String[] copyCommandArr = new String[] {"/usr/bin/pg_createcluster", "13", ID.toString(), 
-				"&&", "rm", "-rf", String.format("/var/lib/postgresql/13/%1$s", ID.toString()),
-				"&&", "cp", "-rf", "/var/lib/postgresql/13/data", String.format("/var/lib/postgresql/13/%1$s", ID.toString())};
-		String copyCommand = String.format(
-				"/usr/bin/pg_createcluster 13 %1$s"/* + 
-				rm -rf /var/lib/postgresql/13/%1$s && " + 
-				"cp -rf /var/lib/postgresql/13/data /var/lib/postgresql/13/%1$s"*/, ID.toString());
+		String[] copyCommandArr = new String[] {"bash", "-c", 
+				String.format("/usr/bin/pg_createcluster 13 %1$s"
+						+ "&& rm -rf /var/lib/postgresql/13/%1$s"
+						+ "&& cp -rf /var/lib/postgresql/13/data /var/lib/postgresql/13/%1$s", ID.toString())}; 
+				//"&&", "rm", "-rf", String.format("/var/lib/postgresql/13/%1$s", ID.toString()),
+				//"&&", "cp", "-rf", "/var/lib/postgresql/13/data", String.format("/var/lib/postgresql/13/%1$s", ID.toString())};
+		//String copyCommand = String.format(
+		//		"/usr/bin/pg_createcluster 13 %1$s"/* + 
+		//		rm -rf /var/lib/postgresql/13/%1$s && " + 
+		//		"cp -rf /var/lib/postgresql/13/data /var/lib/postgresql/13/%1$s"*/, ID.toString());
 		System.out.println(String.format("Testing instance %s", ID.toString()));
 		ProcessBuilder processBuilder = new ProcessBuilder(copyCommandArr);
 		Process copyProcess = processBuilder.start();
-		System.out.println(copyProcess.getInputStream().readAllBytes());
+		System.out.println(String.valueOf(copyProcess.getInputStream().readAllBytes()));
 		try {
 			copyProcess.waitFor();
 		} catch (InterruptedException e) {
