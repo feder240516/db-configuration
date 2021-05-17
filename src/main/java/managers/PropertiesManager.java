d790f8374a3650feccc5cb27b767e53aa6907b3d
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-17 00:34:58 -0500

    Properties Manager

---
+++ src/main/java/managers/PropertiesManager.java
@@ -0,0 +1,41 @@
+package managers;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesManager {
+	private static PropertiesManager _instance = null;
+	private Properties properties;
+	
+	private static final String PROPS_FILENAME = "config.properties";
+	private static final String PROPS_DEFAULT_FILENAME = "config.default.properties";
+	
+	public static PropertiesManager getInstance() {
+		if (_instance == null) {
+			_instance = new PropertiesManager();
+		}
+		return _instance;
+	}
+	
+	public PropertiesManager() {
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPS_FILENAME);
+			InputStream inputStreamDefault = getClass().getClassLoader().getResourceAsStream(PROPS_FILENAME);){
+			properties = new Properties();
+			if (inputStream != null) {
+				properties.load(inputStream);
+			} else if (inputStreamDefault != null) {
+				properties.load(inputStreamDefault);
+			} else {
+				throw new FileNotFoundException("No properties file found in the classpath");
+			}
+		} catch (IOException e) {
+			System.err.println("Couldn't load config.properties.");
+		}
+	}
+	
+	public String getProperty(String key) {
+		return properties.getProperty(key);
+	}
+}
---
+++ src/test/java/managers/PropertiesManagerTest.java
@@ -0,0 +1,39 @@
+package managers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PropertiesManagerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		String derbyLocation = PropertiesManager.getInstance().getProperty("derby.location");
+		if(derbyLocation == null) fail();
+		System.out.println(String.format("Derby location: %s", derbyLocation));
+		
+	}
+
+}
