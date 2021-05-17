ca7fc93b3dc544977c58df8c709204d1f1ef6303
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-17 04:51:48 -0500

    no pass

--- src/main/java/handlers/linux/PostgreSQLHandleLinux.java
+++ src/main/java/handlers/linux/PostgreSQLHandleLinux.java
@@ -65,7 +65,7 @@ public class PostgreSQLHandleLinux extends PostgreSQLHandle {
 	protected String getConnectionString () {
 		String user = PropertiesManager.getInstance().getProperty("postgres.user");
 		String pass = PropertiesManager.getInstance().getProperty("postgres.password");
-		String dbUrl = String.format("jdbc:postgresql://localhost:%d/%s?user=%s&password=&s", port, "employees", user, pass);
+		String dbUrl = String.format("jdbc:postgresql://localhost:%d/%s", port, "employees");
 		return dbUrl;
 	}
 	
