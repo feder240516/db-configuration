80d30c0ccbd81255c091336ddd7c0b648e15d4eb
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-17 02:43:59 -0500

    fixed postgres test

--- src/main/java/handlers/linux/PostgreSQLHandleLinux.java
+++ src/main/java/handlers/linux/PostgreSQLHandleLinux.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import ai.libs.jaicore.components.api.IComponentInstance;
 import exceptions.UnavailablePortsException;
 import handlers.PostgreSQLHandle;
+import managers.PropertiesManager;
 
 public class PostgreSQLHandleLinux extends PostgreSQLHandle {
 
@@ -19,13 +20,7 @@ public class PostgreSQLHandleLinux extends PostgreSQLHandle {
 		String[] copyCommandArr = new String[] {"bash", "-c", 
 				String.format("/usr/bin/pg_createcluster 13 %1$s"
 						+ "&& rm -rf /var/lib/postgresql/13/%1$s"
-						+ "&& sudo -u postgres cp -rf /var/lib/postgresql/13/data /var/lib/postgresql/13/%1$s", ID.toString())}; 
-				//"&&", "rm", "-rf", String.format("/var/lib/postgresql/13/%1$s", ID.toString()),
-				//"&&", "cp", "-rf", "/var/lib/postgresql/13/data", String.format("/var/lib/postgresql/13/%1$s", ID.toString())};
-		//String copyCommand = String.format(
-		//		"/usr/bin/pg_createcluster 13 %1$s"/* + 
-		//		rm -rf /var/lib/postgresql/13/%1$s && " + 
-		//		"cp -rf /var/lib/postgresql/13/data /var/lib/postgresql/13/%1$s"*/, ID.toString());
+						+ "&& sudo -u postgres cp -rf /var/lib/postgresql/13/data /var/lib/postgresql/13/%1$s", ID.toString())};
 		System.out.println(String.format("Testing instance %s", ID.toString()));
 		ProcessBuilder processBuilder = new ProcessBuilder(copyCommandArr);
 		Process copyProcess = processBuilder.start();
@@ -35,6 +30,39 @@ public class PostgreSQLHandleLinux extends PostgreSQLHandle {
 		} catch (InterruptedException e) {
 			throw new IOException("Couldn't create new PostgreSQL instance");
 		}
+	}
+	
+	@Override
+	protected String[] getStartCommand() {
+		String postgresqlHome = PropertiesManager.getInstance().getProperty("postgres.location");
+		System.out.println(String.format("Running in port %d", port));
+		if (postgresqlHome == null || postgresqlHome.equals("")) throw new RuntimeException("Connector location not specified");
+		String[] comandoArray = {"\"" + postgresqlHome + "/bin/pg_ctl" + "\"", "-D", createdInstancePath, "-l", createdInstancePath + "/log.txt", "-o", String.format("\"-F -p %d\"", port), "start"};
+		return comandoArray;
+	}
+
+	@Override
+	public void stopServer() {
+		System.out.println("Stopping server");
+		try {
+			String postgresqlHome = PropertiesManager.getInstance().getProperty("postgres.location");
+			String[] comandoArray = {postgresqlHome + "/bin/pg_ctl", "-D", createdInstancePath, "-l", createdInstancePath + "/log.txt", "stop"};
+			ProcessBuilder processBuilder = new ProcessBuilder(comandoArray);
+			processBuilder.start().waitFor();
+			
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected String getInstancesPath() {
+		return PropertiesManager.getInstance().getProperty("postgres.instances.location");
+	}
+
+	@Override
+	protected String getBasePath() {
+		return PropertiesManager.getInstance().getProperty("postgres.data.location");
 	}
 
 }
--- src/main/java/managers/PropertiesManager.java
+++ src/main/java/managers/PropertiesManager.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.lang3.SystemUtils;
+
 public class PropertiesManager {
 	private static PropertiesManager _instance = null;
 	private Properties properties;
 	
 	private static final String PROPS_FILENAME = "config.properties";
 	private static final String PROPS_DEFAULT_FILENAME = "config.default.properties";
+	private static final String PROPS_LINUX_DEFAULT_FILENAME = "config.linux.default.properties";
+	private static final String PROPS_WINDOWS_DEFAULT_FILENAME = "config.windows.default.properties";
 	
 	public static PropertiesManager getInstance() {
 		if (_instance == null) {
@@ -21,10 +25,16 @@ public class PropertiesManager {
 	
 	public PropertiesManager() {
 		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPS_FILENAME);
-			InputStream inputStreamDefault = getClass().getClassLoader().getResourceAsStream(PROPS_FILENAME);){
+			InputStream inputStreamDefault = getClass().getClassLoader().getResourceAsStream(PROPS_DEFAULT_FILENAME);
+			InputStream inputStreamLinuxDefault = getClass().getClassLoader().getResourceAsStream(PROPS_DEFAULT_FILENAME);
+			InputStream inputStreamWindowsDefault = getClass().getClassLoader().getResourceAsStream(PROPS_DEFAULT_FILENAME);){
 			properties = new Properties();
 			if (inputStream != null) {
 				properties.load(inputStream);
+			} else if (SystemUtils.IS_OS_LINUX && inputStreamLinuxDefault != null) {
+				properties.load(inputStreamDefault);
+			} else if (SystemUtils.IS_OS_WINDOWS && inputStreamWindowsDefault != null) {
+				properties.load(inputStreamDefault);
 			} else if (inputStreamDefault != null) {
 				properties.load(inputStreamDefault);
 			} else {
