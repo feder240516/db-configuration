aa770b83c941f24c9e6a4292d62195209fa1d694
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-18 00:59:50 -0500

    final fix of mariadb

--- src/test/java/handlers/MariaDBHandleTest.java
+++ src/test/java/handlers/MariaDBHandleTest.java
@@ -50,7 +50,6 @@ public class MariaDBHandleTest {
 	@Test
 	void test() throws ClassNotFoundException, UnavailablePortsException, IOException, SQLException, InterruptedException {
 		IComponent maria = new Component("MariaDB");
-		System.out.println("vah");
 		IComponentInstance mariainst = new ComponentInstance(maria, new HashMap<>(), new HashMap<>());
 		ADatabaseHandle mariaHandle = DBSystemFactory.getInstance().createHandle(mariainst);
 		System.out.println("Finally connected");
