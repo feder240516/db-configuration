7693a88211aa25424f6d094cd2fd8202d6d146cf
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-18 00:07:14 -0500

    unused 7

--- src/test/java/small/BashTest.java
+++ src/test/java/small/BashTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class BashTest {
 	@Test
 	public void main() throws IOException, InterruptedException {
-		String[] command = new String[] {"bash", "-c", "\"sudo /usr/sbin/mysqld --datadir=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967 --port=9901 --socket=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967/mysql.sock --query-cache-type=0 --query-cache-size=0\""};
+		String[] command = new String[] {"bash", "-c", "\"sudo -u root /usr/sbin/mysqld --datadir=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967 --port=9901 --socket=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967/mysql.sock --query-cache-type=0 --query-cache-size=0\""};
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.redirectOutput(new File("/home/ailibs/output.txt"));
 		pb.redirectError(new File("/home/ailibs/error.txt"));
