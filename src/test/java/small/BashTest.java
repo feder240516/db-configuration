fbf6d6702e26e51aefe1f6ff4be988b3043bb212
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-17 23:51:39 -0500

    unused 2

--- src/test/java/small/BashTest.java
+++ src/test/java/small/BashTest.java
@@ -10,7 +10,7 @@ public class BashTest {
 	@Test
 	public void main() throws IOException {
 		Scanner sc = new Scanner(System.in);
-		String command = sc.nextLine();
+		String command = "bash -c \"sudo /usr/sbin/mysqld --datadir=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967 --port=9901 --socket=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967/mysql.sock --query-cache-type=0 --query-cache-size=0\"";
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.redirectOutput(new File("/home/ailibs/output.txt"));
 		pb.start();
