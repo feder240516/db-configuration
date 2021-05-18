1548f244634ce107bf1046665bdccd85df6136a2
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-18 00:00:09 -0500

    unused 6

--- src/test/java/small/BashTest.java
+++ src/test/java/small/BashTest.java
@@ -11,7 +11,7 @@ public class BashTest {
 		String[] command = new String[] {"bash", "-c", "\"sudo /usr/sbin/mysqld --datadir=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967 --port=9901 --socket=/home/ailibs/DBInstances/MariaDB/2a92eba2-6eaa-489d-b68e-ab351f8d5967/mysql.sock --query-cache-type=0 --query-cache-size=0\""};
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.redirectOutput(new File("/home/ailibs/output.txt"));
-		pb.redirectError((new File("/home/ailibs/error.txt");
+		pb.redirectError(new File("/home/ailibs/error.txt"));
 		Process p = pb.start();
 		p.waitFor();
 	}
