c8a6c6a2193960a9dbba09febf1a820aa072274c
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-17 23:49:38 -0500

    unused

--- src/test/java/small/BashTest.java
+++ src/test/java/small/BashTest.java
@@ -4,12 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.junit.jupiter.api.Test;
+
 public class BashTest {
-	public static void main(String[] args) throws IOException {
+	@Test
+	public void main() throws IOException {
 		Scanner sc = new Scanner(System.in);
 		String command = sc.nextLine();
 		ProcessBuilder pb = new ProcessBuilder(command);
-		pb.redirectOutput(new File("home/ailibs/output.txt"));
+		pb.redirectOutput(new File("/home/ailibs/output.txt"));
 		pb.start();
 	}
 }
