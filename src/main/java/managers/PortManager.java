06d7714c82e4a51459131f515c43f724f9e4dfb7
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-20 01:26:33 -0500

    changed port policy

--- src/main/java/managers/PortManager.java
+++ src/main/java/managers/PortManager.java
@@ -2,8 +2,10 @@ package managers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import exceptions.UnavailablePortsException;
 import helpers.Port;
@@ -16,20 +18,24 @@ public class PortManager {
 	}
 	
 	private Map<Integer,Port> ports = new HashMap<>();
-	private int[] portsNumbers;
+	private Queue<Integer> portsQueue = new LinkedList<>();
 	
 	public void setupAvailablePorts(int[] newPorts) {
 		for(int port: newPorts) {
-			if (!ports.containsKey(port)) { ports.put(port,new Port(port)); }
+			if (!ports.containsKey(port)) { 
+				ports.put(port,new Port(port));
+				portsQueue.add(port);
+			}
 		}
 	}
 	
 	public synchronized int acquireAnyPort() throws UnavailablePortsException {
-		for(Port port: ports.values()) {
-			if (port.available) {
-				port.available = false;
-				return port.number;
-			}
+		int portNumber = portsQueue.remove();
+		portsQueue.add(portNumber);
+		int triedPorts = 0;
+		while(!ports.get(portNumber).available && ++triedPorts < ports.size()) {
+			portNumber = portsQueue.remove();
+			portsQueue.add(portNumber);
 		}
 		throw new UnavailablePortsException("There are no available ports");
 	}
