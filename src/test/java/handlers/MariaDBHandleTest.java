105a7cce8b32f36362bb341aabe291d407f0960a
Federico Reina <federicoreinavinchira@yahoo.com>
2021-05-23 01:44:51 -0500

    two tests for mariadb

--- src/test/java/handlers/MariaDBHandleTest.java
+++ src/test/java/handlers/MariaDBHandleTest.java
@@ -1,11 +1,18 @@
 package handlers;
 
+import static org.jooq.impl.DSL.extract;
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.select;
+import static org.jooq.impl.DSL.val;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+import org.jooq.DatePart;
+import org.jooq.Query;
+import org.jooq.SQLDialect;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -47,6 +54,17 @@ public class MariaDBHandleTest {
 	void tearDown() throws Exception {
 	}
 	
+	public Query getTestQuery() {
+		return select(field("employees.emp_no"), 
+				field("employees.first_name"), 
+				field("employees.last_name"), 
+				field("salaries.salary"))
+		.from("employees")
+		.join("salaries")
+		.on(field("employees.emp_no").eq(field("salaries.emp_no")))
+		.where(extract(field("salaries.to_date"),DatePart.YEAR).eq(val(9999)));
+	}
+	
 	@Test
 	void test() throws ClassNotFoundException, UnavailablePortsException, IOException, SQLException, InterruptedException {
 		IComponent maria = new Component("MariaDB");
@@ -56,6 +74,22 @@ public class MariaDBHandleTest {
 		mariaHandle.initiateServer();
 		mariaHandle.printResultsAfterExecution(true);
 		double executionTime = mariaHandle.benchmarkQuery("select count(*) from employees");
+		System.out.println(String.format("query was executed in %f miliseconds", executionTime));
+		mariaHandle.stopServer();
+		mariaHandle.cleanup(); 
+	}
+	
+	@Test
+	void test2() throws ClassNotFoundException, UnavailablePortsException, IOException, SQLException, InterruptedException {
+		IComponent maria = new Component("MariaDB");
+		IComponentInstance mariainst = new ComponentInstance(maria, new HashMap<>(), new HashMap<>());
+		ADatabaseHandle mariaHandle = DBSystemFactory.getInstance().createHandle(mariainst);
+		System.out.println("Finally connected");
+		mariaHandle.initiateServer();
+		mariaHandle.printResultsAfterExecution(false);
+		Query query = getTestQuery();
+		query.configuration().set(SQLDialect.MARIADB);
+		double executionTime = mariaHandle.benchmarkQuery(query.getSQL(true));
 		System.out.println(String.format("query was executed in %f miliseconds", executionTime));
 		mariaHandle.stopServer();
 		mariaHandle.cleanup(); 
