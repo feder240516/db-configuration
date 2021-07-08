d45919e2b3ce5d86a6dddacc053eb431fe0671d8
Federico Reina <federicoreinavinchira@gmail.com>
2021-07-08 15:23:25 -0500

    test for bash

---
+++ src/test/java/commands/BashCommandTest.java
@@ -0,0 +1,40 @@
+package commands;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BashCommandTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() throws IOException, InterruptedException {
+		ProcessBuilder pb = new ProcessBuilder(new String[] {
+			"/bin/bash", "-c", "echo \"hello world\""
+		});
+		Process process = pb.start();
+		process.waitFor();
+	}
+
+}
