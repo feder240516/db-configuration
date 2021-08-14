63c4361a51ff708330cf870a94c5f841c6792089
Federico Reina <federicoreinavinchira@gmail.com>
2021-08-14 16:55:11 -0500

    script for loading mariadb

---
+++ sql/mariadb/load.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/bin/bash -c "mysqld --datadir=\"/usr/local/bin/DBInstances/MariaDB/data\" --port=\"9001\"" \
+    --socket="/usr/local/bin/DBInstances/MariaDB/data/mysql.sock" \
+    --pid-file="/usr/local/bin/DBInstances/MariaDB/data/mysql.pid" --skip-grant-tables &
+sleep 5
+#mysql -u root -e "CREATE DATABASE mydb"
+#mysql -u root mydb < /tmp/dump.sql
