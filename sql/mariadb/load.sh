13207567c91fd5e15f94ab8bc506117cea86e823
Federico Reina <federicoreinavinchira@gmail.com>
2021-08-14 17:20:34 -0500

    sql command

--- sql/mariadb/load.sh
+++ sql/mariadb/load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/bash -c "mysqld --datadir=\"/usr/local/bin/DBInstances/MariaDB/data\" --port=\"9001\"" \
+mysqld --datadir="/usr/local/bin/DBInstances/MariaDB/data" --port="9001" \
     --socket="/usr/local/bin/DBInstances/MariaDB/data/mysql.sock" \
     --pid-file="/usr/local/bin/DBInstances/MariaDB/data/mysql.pid" --skip-grant-tables &
 sleep 5
