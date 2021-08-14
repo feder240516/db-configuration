e0db6c7dfc82ba5823d89c4ca3de5e102ffb4454
Federico Reina <federicoreinavinchira@gmail.com>
2021-08-14 17:00:04 -0500

    finally adding data mariadb

--- sql/mariadb/load.sh
+++ sql/mariadb/load.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 /bin/bash -c "mysqld --datadir=\"/usr/local/bin/DBInstances/MariaDB/data\" --port=\"9001\"" \
     --socket="/usr/local/bin/DBInstances/MariaDB/data/mysql.sock" \
-    --pid-file="/usr/local/bin/DBInstances/MariaDB/data/mysql.pid" --skip-grant-tables
+    --pid-file="/usr/local/bin/DBInstances/MariaDB/data/mysql.pid" --skip-grant-tables &
 sleep 5
-#mysql -u root -e "CREATE DATABASE mydb"
-#mysql -u root mydb < /tmp/dump.sql
+mysql --socket="/usr/local/bin/DBInstances/MariaDB/data/mysql.sock" < /usr/local/bin/db-configuration/sql/mariadb/employees-mariadb.sql
