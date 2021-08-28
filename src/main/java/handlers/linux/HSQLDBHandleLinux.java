c76d9af684304518965af0b540a5a8f8e256732a
Federico Reina <federicoreinavinchira@gmail.com>
2021-08-27 21:39:44 -0500

    fix typo

--- src/main/java/handlers/linux/HSQLDBHandleLinux.java
+++ src/main/java/handlers/linux/HSQLDBHandleLinux.java
@@ -20,7 +20,7 @@ public class HSQLDBHandleLinux extends HSQLDBHandle {
 	@Override
 	protected String[] getStartCommand() {
 		String HSQLDBHome = PropertiesManager.getInstance().getProperty("hsqldb.location");
-		String dbName = "employees"
+		String dbName = "employees";
 		String[] cmdStart = {"/bin/bash", "-c", String.format("java -cp %1$s/lib/hsqldb.jar org.hsqldb.Server -database.%3$s --dbname.%3$s file:%3$s xdb -port %2$s", HSQLDBHome, port, employees)};
 		return cmdStart;
 	}
