bb502243a6b2066c9bb4c455ca4d3d43d58583d5
Federico Reina <federicoreinavinchira@gmail.com>
2021-09-04 18:07:14 -0500

    fixed typo error in hsql script

--- sql/hsqldb/load.sh
+++ sql/hsqldb/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DB_SCRIPT_FILE="/usr/local/bin/db-configuration/sql/employees-hsqldb.script"
-RUN cp /usr/local/bin/db-configuration/sql/hsqldb/$DB_SCRIPT_FILE /usr/local/bin/DBInstances/HSQLDB/data/employees.script
+RUN cp $DB_SCRIPT_FILE /usr/local/bin/DBInstances/HSQLDB/data/employees.script
