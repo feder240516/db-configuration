b0294d3da67eee58993e62ed063afd4de3615b24
Federico Reina <federicoreinavinchira@gmail.com>
2021-09-05 14:13:11 -0500

    fix hsql script

--- sql/hsqldb/load.sh
+++ sql/hsqldb/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DB_SCRIPT_FILE="/usr/local/bin/db-configuration/sql/hsqldb/employees-hsqldb.script"
-RUN cp $DB_SCRIPT_FILE /usr/local/bin/DBInstances/HSQLDB/data/employees.script
+cp $DB_SCRIPT_FILE /usr/local/bin/DBInstances/HSQLDB/data/employees.script
