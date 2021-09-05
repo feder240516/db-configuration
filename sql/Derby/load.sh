7eac33cc49cf03c6f0539ace18c15065587adac8
Federico Reina <federicoreinavinchira@gmail.com>
2021-09-04 19:05:32 -0500

    fix derby typo

--- sql/Derby/load.sh
+++ sql/Derby/load.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /usr/local/bin/DBInstances/Derby/data
-echo "ap path = "
-pwd
 DB_SCRIPT_FILE="/usr/local/bin/db-configuration/sql/employees-apacheDerby.sql"
 #java -jar %DERBY_HOME%/lib/derbyrun.jar server start &
-java -jar /opt/Apache/lib/derbyrun.jar ij $DB_SCRIPT_FILE
+java -jar /opt/Apache/db-derby-10.15.2.0-bin/lib/derbyrun.jar ij $DB_SCRIPT_FILE
 #sleep 5
 #mysql -P 9001 --protocol="tcp" --socket="/usr/local/bin/DBInstances/MariaDB/data/mysql.sock" < /usr/local/bin/db-configuration/sql/mariadb/employees-mariadb.sql
